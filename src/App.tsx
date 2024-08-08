48df4dc0f671f83534f0a7c112476cd289abb345
Денис Касперович <kasperovich.deonis@gmail.com>
2024-08-08 14:39:02 +0300

    getting the current time

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(()=>{
+    setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+  }, [])
+
+
+    const convertsToTwoDigitValue = (date: number) => {
+      if(date < 10){
+          return '0' + date
+      }
+      return date
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{convertsToTwoDigitValue(date.getHours())}</span><span> : </span>
+      <span>{convertsToTwoDigitValue(date.getMinutes())}</span><span> : </span>
+      <span>{convertsToTwoDigitValue(date.getSeconds())}</span>
     </div>
   );
 }
